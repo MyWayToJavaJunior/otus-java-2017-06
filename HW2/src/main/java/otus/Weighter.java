2c9b6fba118779125c487064c3352d92dbbbe831
Danil <infatigabilis@gmail.com>
2017-06-15 18:54:27 +1000

    fix hw2

--- HW2/src/main/java/otus/Weighter.java
+++ HW2/src/main/java/otus/Weighter.java
@@ -1,22 +1,22 @@
 package otus;
 
+import java.util.function.Supplier;
+
 public class Weighter {
     private final int size = 1_000_000;
     private Runtime runtime = Runtime.getRuntime();
 
-    interface Action {
-        Object exec();
-    }
-
-    public int run(Action action) throws InterruptedException {
+    public int run(Supplier<Object> supplier) throws InterruptedException {
         int n = 10;
         float result = 0.0f;
 
         for (int i = 0; i < n; i++) {
             Object array[] = new Object[size];
 
+            System.gc();
+
             long startMem = runtime.totalMemory() - runtime.freeMemory();
-            for (int j = 0; j < size; j++) array[j] = action.exec();
+            for (int j = 0; j < size; j++) array[j] = supplier.get();
             long endMem = runtime.totalMemory() - runtime.freeMemory();
 
             result += (float)(endMem - startMem) / size;
