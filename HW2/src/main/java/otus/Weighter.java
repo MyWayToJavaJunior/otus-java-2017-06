d8f5023ab44e6f4ed7526fbba606de3ec4245589
Danil <infatigabilis@gmail.com>
2017-06-15 21:28:55 +1000

    fix hw2 (2)

--- HW2/src/main/java/otus/Weighter.java
+++ HW2/src/main/java/otus/Weighter.java
@@ -5,26 +5,27 @@ import java.util.function.Supplier;
 public class Weighter {
     private final int size = 1_000_000;
     private Runtime runtime = Runtime.getRuntime();
+    Object[] A;
 
     public int run(Supplier<Object> supplier) throws InterruptedException {
         int n = 10;
         float result = 0.0f;
 
         for (int i = 0; i < n; i++) {
-            Object array[] = new Object[size];
+            A = new Object[size];
 
-            System.gc();
-
-            long startMem = runtime.totalMemory() - runtime.freeMemory();
-            for (int j = 0; j < size; j++) array[j] = supplier.get();
-            long endMem = runtime.totalMemory() - runtime.freeMemory();
+            long startMem = getMem();
+            for (int j = 0; j < size; j++) A[j] = supplier.get();
+            long endMem = getMem();
 
             result += (float)(endMem - startMem) / size;
-
-            System.gc();
-            Thread.sleep(10);
         }
 
         return Math.round(result / n);
+    }
+
+    private long getMem() {
+        System.gc();
+        return runtime.totalMemory() - runtime.freeMemory();
     }
 }
