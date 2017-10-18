0e50c1b8e887d363cd25143615f883d630e47153
infatigabilis <infatigabilis@gmail.com>
2017-10-18 20:07:10 +1000

    remove useless write thread of NIO Server

--- HW16 - Multi-process Message System/src/main/java/hw16/socket/SocketMessageServer.java
+++ HW16 - Multi-process Message System/src/main/java/hw16/socket/SocketMessageServer.java
@@ -24,25 +24,21 @@ import java.util.logging.Logger;
 public class SocketMessageServer {
     private static final Logger logger = Logger.getLogger(SocketMessageServer.class.getName());
 
-    private static final int THREADS_NUMBER = 1;
     public static final int PORT = 8080;
-    private static final int ECHO_DELAY = 100;
     private static final int CAPACITY = 10;
     private static final String MESSAGES_SEPARATOR = "\n\n";
 
     public static final Address DB_ADDRESS = new Address("db");
 
-    private final ExecutorService executor = Executors.newFixedThreadPool(THREADS_NUMBER);
-    private final Map<String, ChannelMessages> channelMessages = new ConcurrentHashMap<>();
-    private final Map<Address, ChannelMessages> addresses = new ConcurrentHashMap<>();
-    private final Queue<Address> dbAddresses = new ConcurrentLinkedQueue<>();
+    private final Map<String, SocketChannel> channels = new HashMap<>();
+    private final Map<Address, SocketChannel> addresses = new HashMap<>();
+    private final Queue<Address> dbAddresses = new LinkedList<>();
 
     private StringBuilder readBuilder = new StringBuilder();
 
 
     @SuppressWarnings("InfiniteLoopStatement")
     public void start() throws Exception {
-        executor.submit(this::write);
 
         try (ServerSocketChannel serverSocketChannel = ServerSocketChannel.open()) {
             serverSocketChannel.bind(new InetSocketAddress("localhost", PORT));
@@ -86,7 +82,7 @@ public class SocketMessageServer {
         channel.configureBlocking(false);
         channel.register(selector, SelectionKey.OP_READ);
 
-        channelMessages.put(remoteAddress, new ChannelMessages(channel));
+        channels.put(remoteAddress, channel);
     }
 
     private void read(SelectionKey key) throws IOException, ParseException, ClassNotFoundException {
@@ -99,52 +95,34 @@ public class SocketMessageServer {
             readBuilder.append(part);
 
             if (part.length() != read) {
-                readMessage(channel);
-                return;
+                Message msg = readMessage(channel);
+                if (msg != null) writeMessage(msg);
             }
         } else {
             key.cancel();
             String remoteAddress = channel.getRemoteAddress().toString();
-            channelMessages.remove(remoteAddress);
+            channels.remove(remoteAddress);
             logger.info("Connection closed, key canceled");
         }
     }
 
-    private void readMessage(SocketChannel channel) throws ParseException, ClassNotFoundException, IOException {
+    private Message readMessage(SocketChannel channel) throws ParseException, ClassNotFoundException, IOException {
         Message msg = Message.getMsgFromJSON(readBuilder.toString());
         readBuilder = new StringBuilder();
 
         logger.info("Message received: " + msg + " from: " + channel.getRemoteAddress());
 
         if (msg instanceof AddressMessage) {
-            addresses.put(msg.getFrom(), channelMessages.get(channel.getRemoteAddress().toString()));
+            addresses.put(msg.getFrom(), channels.get(channel.getRemoteAddress().toString()));
             logger.info("Accept " + msg.getFrom());
 
             if ((msg.getFrom().getId().split("@")[0]).equals(DB_ADDRESS.getId())) {
                 dbAddresses.add(msg.getFrom());
             }
-        }
-        else channelMessages.get(channel.getRemoteAddress().toString()).messages.add(msg);
-    }
 
-    @SuppressWarnings("InfiniteLoopStatement")
-    private Object write() throws InterruptedException {
-        while (true) {
-            for (Map.Entry<String, ChannelMessages> entry : channelMessages.entrySet()) {
-                ChannelMessages channelMessages = entry.getValue();
-                if (channelMessages.channel.isConnected()) {
-                    channelMessages.messages.forEach(message -> {
-                        try {
-                            writeMessage(message);
-                        } catch (IOException e) {
-                            logger.log(Level.SEVERE, e.getMessage());
-                        }
-                    });
-                    channelMessages.messages.clear();
-                }
-            }
-            Thread.sleep(ECHO_DELAY);
+            return null;
         }
+        else return msg;
     }
 
     private void writeMessage(Message message) throws IOException {
@@ -153,19 +131,10 @@ public class SocketMessageServer {
         String json = new Gson().toJson(message) + MESSAGES_SEPARATOR;
         ByteBuffer buffer = ByteBuffer.wrap(json.getBytes());
 
-        logger.info("Send message" + message + ", to " + to + " (" + addresses.get(to).channel.getRemoteAddress() + ")");
+        logger.info("Send message" + message + ", to " + to + " (" + addresses.get(to).getRemoteAddress() + ")");
 
         while (buffer.hasRemaining()) {
-            addresses.get(to).channel.write(buffer);
-        }
-    }
-
-    private class ChannelMessages {
-        private final SocketChannel channel;
-        private final List<Message> messages = new ArrayList<>();
-
-        private ChannelMessages(SocketChannel channel) {
-            this.channel = channel;
+            addresses.get(to).write(buffer);
         }
     }
 
